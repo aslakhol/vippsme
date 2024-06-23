a3cf9ab001fec0a8811837084373c6cf7784906b
Aslak Hollund <aslakhol@gmail.com>
2024-06-23 13:05:43 +0200

    fix error messages

--- src/components/Vipps.tsx
+++ src/components/Vipps.tsx
@@ -114,14 +114,24 @@ export const Vipps = () => {
 };
 
 const formSchema = z.object({
-  phone: z.string().length(8, "Må være 8 siffer").optional(),
+  phone: z
+    .string()
+    .refine((v) => {
+      return !v.includes("+") && !v.startsWith("00");
+    }, "Ikke bruk landskode (f.eks. +47).")
+    .refine((v) => {
+      return !isNaN(+v);
+    }, "Må være bare tall.")
+    .refine((v) => {
+      return v.length === 8;
+    }, "Må være 8 siffer."),
   amount: z.coerce
     .number()
-    .min(0, "Må være et positivt tall")
-    .max(99000, "Vipps tillater ikke beløp over 99 000 kr")
+    .min(0, "Må være et positivt tall.")
+    .max(99000, "Vipps tillater ikke beløp over 99 000 kr.")
     .optional(),
   message: z
     .string()
-    .max(50, "Vipps tillater ikke meldinger lengre en 50 tegn")
+    .max(50, "Vipps tillater ikke meldinger lengre en 50 tegn.")
     .optional(),
 });
