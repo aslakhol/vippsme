bb16ff5d9d164ed08ce0a7d7f5332e3b7cbc1197
Aslak Hollund <aslakhol@gmail.com>
2024-06-24 16:21:53 +0200

    padding

--- src/app/v/page.tsx
+++ src/app/v/page.tsx
@@ -6,7 +6,7 @@ export default async function SlugPage({
   searchParams: { p: string; a: string; m: string };
 }) {
   return (
-    <main className="mx-auto flex h-screen max-w-sm flex-col items-start gap-8 py-20">
+    <main className="mx-auto flex h-screen max-w-sm flex-col items-start gap-8 px-4 py-20">
       {
         <Redirect
           phone={searchParams.p}
